046e5526ecc38a62fee3985a77635fca44e0130a
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-02-16 16:33:25 +0100

    Refactor error messages

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -15,27 +15,27 @@ class NetworkManager {
     let baseURL = "https://api.github.com/users/"
     let perPageFollowers = "?per_page=100"
     
-    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String?) -> Void)  {
+    func getFollowers(for username: String, page: Int, completed: @escaping ([Follower]?, ErrorMessage?) -> Void)  {
         let endpoint = baseURL + "\(username)/followers\(perPageFollowers)&page=\(page)"
         
         guard let url = URL(string: endpoint) else {
-            completed(nil, "This username created an invalid request. Please try again.")
+            completed(nil, .invalidUsername)
             return
         }
         
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             
             if let _ = error {  // if the error exists
-                completed(nil, "Unable to complete your request. Please check your internet connection.")
+                completed(nil, .unableToComplete)
             }
             
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {    // https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-                completed(nil, "Invalid response from the server. Please try again.")
+                completed(nil, .invalidResponse)
                 return
             }
             
             guard let data = data else {
-                completed(nil, "The data received from the server is invalid. Please try again.")
+                completed(nil, .invalidData)
                 return
             }
             
@@ -47,7 +47,7 @@ class NetworkManager {
                 completed(followers, nil)
                 
             } catch {
-                completed(nil, "The data received from the server was invalid. Please try again.")
+                completed(nil, .invalidData )
             }
         }
         
