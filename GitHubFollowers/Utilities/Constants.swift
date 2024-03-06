7769ca2273ebd26aba4a55454221af22c33bb9ce
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-03-07 00:47:56 +0100

    Create ItemInfoView

--- GitHubFollowers/Utilities/Constants.swift
+++ GitHubFollowers/Utilities/Constants.swift
@@ -8,5 +8,10 @@
 import Foundation
 
 enum SFSymbols {
-    static let location = "mappin.and.ellipse"
+    static let location     = "mappin.and.ellipse"
+    
+    static let repos        = "folder"
+    static let gists        = "text.allignleft"
+    static let followers    = "person.2"
+    static let following    = "heart"
 }
