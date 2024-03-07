c9dfa1b646bfea00b57cbb98912ed63ae129ce59
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-03-07 12:46:41 +0100

    Add custom init to access user in ItemInfoVC

--- GitHubFollowers/Custom Views/ViewControllers/GFItemInfoVC.swift
+++ GitHubFollowers/Custom Views/ViewControllers/GFItemInfoVC.swift
@@ -13,7 +13,19 @@ class GFItemInfoVC: UIViewController {
     let itemInfoViewOne = GFItemInfoView()
     let itemInfoViewTwo = GFItemInfoView()
     let actionButton    = GFButton()
-
+    
+    var user: User!
+    
+    //init with user so we can assign all user stuff to UI components
+    init(user: User) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureBackgroudView()
---
+++ GitHubFollowers/Custom Views/ViewControllers/GFRepoItemVC.swift
@@ -0,0 +1,8 @@
+//
+//  GFRepoItemVC.swift
+//  GitHubFollowers
+//
+//  Created by Paweł Rudnik on 07/03/2024.
+//
+
+import Foundation
