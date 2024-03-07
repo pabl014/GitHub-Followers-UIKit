ce21dd6de272fa8bdf06b99d035225758f9f9bd4
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-03-07 13:09:35 +0100

    Quick bugfix: fix actionButton bottomAnchor constraint

--- GitHubFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFItemInfoVC.swift
+++ GitHubFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFItemInfoVC.swift
@@ -62,7 +62,7 @@ class GFItemInfoVC: UIViewController {
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
             stackView.heightAnchor.constraint(equalToConstant: 50),
             
-            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: padding),
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding),
             actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
             actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
             actionButton.heightAnchor.constraint(equalToConstant: 44)
