0d2515a7fac3340540f041b97869fb7325c1fe2a
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-03-08 19:37:36 +0100

    Add moving to FollowerListVC after selecting someone in FavoritesListVC. Add swipe-to-delete functionality

--- GitHubFollowers/Screens/FavoritesListVC.swift
+++ GitHubFollowers/Screens/FavoritesListVC.swift
@@ -83,6 +83,29 @@ extension FavoritesListVC: UITableViewDataSource, UITableViewDelegate {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let favorite = favorites[indexPath.row]
+        let destVC   = FollowerListVC()
+        destVC.username = favorite.login
+        destVC.title    = favorite.login
+        
+        navigationController?.pushViewController(destVC, animated: true)
+    }
     
-    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        guard editingStyle == .delete else { return }
+        
+        let favorite = favorites[indexPath.row]
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left) // when you swipe to the left, a delete option will appear
+        
+        PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+            guard let self = self else { return }
+            
+            guard let error else { return }
+            
+            self.presentGFAlertOnMainThread(title: "Unable to remove", message: error.rawValue, buttonTitle: "Ok")
+        }
+    }
 }
