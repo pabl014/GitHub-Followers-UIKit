65a3a4e57fef55efec9fa061b2f74f69438a548b
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-03-08 11:27:20 +0100

    Create add button in the right upper corner of the FollowerListVC

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -45,6 +45,10 @@ class FollowerListVC: UIViewController {
     func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        navigationItem.rightBarButtonItem = addButton
+    
     }
     
     func configureCollectionView() {
@@ -112,6 +116,10 @@ class FollowerListVC: UIViewController {
         DispatchQueue.main.async {
             self.dataSource.apply(snapshot, animatingDifferences: true) // apply to the snapshot
         }
+    }
+    
+    @objc func addButtonTapped() {
+        print("add button tapped")
     }
 }
 
