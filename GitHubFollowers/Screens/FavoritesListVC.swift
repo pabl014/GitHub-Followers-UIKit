f6114e5137458778945865f3f95b7b4a98fadc6c
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-03-08 19:19:38 +0100

    Add displaying FavoritesCells in tableView in FavoritesListVC

--- GitHubFollowers/Screens/FavoritesListVC.swift
+++ GitHubFollowers/Screens/FavoritesListVC.swift
@@ -8,19 +8,81 @@
 import UIKit
 
 class FavoritesListVC: UIViewController {
-
+    
+    let tableView = UITableView()
+    var favorites: [Follower] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        configureViewController()
+        configureTableView()
+    }
+    
+    // !!! viewDidLoad can only get called once. If the user goes to the FavoritesListVC and sees, that he has no favorites,
+    // then he tabs over to follower list screen, add a favorite and then tab back.
+    // ViewDidLoad will not be called again. That is why we should use viewWillAppear to properly update UI in this case.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getFavorites()
+    }
+    
+    func configureViewController() {
+        view.backgroundColor    = .systemBackground
+        title                   = "Favorites"
+        navigationController?.navigationBar.prefersLargeTitles = true // we have same thing in FollowerListVC in the same func, but we have to write it here, because FollowerListVC and FavoritesListVC are separate Navigation Controllers (check SceneDelegate.swift)
+    }
+    
+    func configureTableView() {
+        view.addSubview(tableView)
         
-        PersistenceManager.retrieveFavorites { result in
+        tableView.frame         = view.bounds
+        tableView.rowHeight     = 80
+        
+        tableView.delegate      = self
+        tableView.dataSource    = self
+        
+        tableView.register(FavoritesCell.self, forCellReuseIdentifier: FavoritesCell.reuseID)
+    }
+    
+    func getFavorites() {
+        PersistenceManager.retrieveFavorites { [weak self] result in
+            guard let self = self else { return }
+            
             switch result {
-                
-            case .success(let favorites):
-                print(favorites)
-            case .failure(let error):
-                break
+                case .success(let favorites):
+                    if favorites.isEmpty {
+                        self.showEmptyStateView(with: "No favorites \n Add one on the follower screen", in: self.view)
+                    } else {
+                        self.favorites = favorites
+                        DispatchQueue.main.async {
+                            self.tableView.reloadData() // reloading data in tableView always on main thread !!!
+                            self.view.bringSubviewToFront(self.tableView) // just in case, to make sure tableView is showing on top, not an empty state
+                        }
+                    }
+                    
+                case .failure(let error):
+                    self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
             }
         }
     }
+}
+
+extension FavoritesListVC: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoritesCell.reuseID) as! FavoritesCell
+        let favorite = favorites[indexPath.row]
+        
+        cell.set(favorite: favorite)
+        
+        return cell
+    }
+    
+    
+    
 }
