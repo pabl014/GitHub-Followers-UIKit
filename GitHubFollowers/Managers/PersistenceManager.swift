a3aa23276c33fc31dc20e12ccc973133b178a888
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-03-08 13:00:45 +0100

    Create PersistenceManager which deals with adding and removing user to favorites

---
+++ GitHubFollowers/Managers/PersistenceManager.swift
@@ -0,0 +1,88 @@
+//
+//  PersistenceManager.swift
+//  GitHubFollowers
+//
+//  Created by Paweł Rudnik on 08/03/2024.
+//
+
+import Foundation
+
+// lets us know whether we are adding or removing follower from favorites
+enum PersistanceActionType {
+    case add, remove
+}
+
+// enum but not struct, because you can init an empty struct, but you cannot init empty enum
+
+enum PersistenceManager {
+    
+    static private let defaults = UserDefaults.standard
+    
+    enum Keys {
+        static let favorites = "favorites"
+    }
+    
+    // passing the follower, actionType: adding or removing, completed: if sth went wrong present an error in ViewController
+    static func updateWith(favorite: Follower, actionType: PersistanceActionType, completed: @escaping (GFError?) -> Void) {
+        retrieveFavorites { result in
+            
+            switch result {
+                
+                case .success(let favorites):
+                    var retrievedFavorites = favorites
+                    
+                    switch actionType {
+                        
+                        case .add:
+                            guard !retrievedFavorites.contains(favorite) else {
+                                // if we already have that guy taht we pass in in line 26 in favorites
+                                completed(.alreadyInFavorites)
+                                return
+                            }
+                            
+                            retrievedFavorites.append(favorite)
+                        
+                        case .remove:
+                            retrievedFavorites.removeAll { $0.login == favorite.login } // iterating through the array, anywhere the login equals the favorite login, we're going to remove all instances of it
+                    }
+                    
+                    completed(save(favorites: retrievedFavorites))
+                
+                case .failure(let error):
+                    completed(error) // completion handler from line 26 (completed: @escaping (GFError?) -> Void)
+            }
+        }
+    }
+    
+    static func retrieveFavorites(completed: @escaping (Result<[Follower], GFError>) -> Void) {
+        
+        guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
+            completed(.success([])) // not returning an error but an empty array, because if this favoritesData is nil, it means that we've never saved anything to favorites before (example of the very first time you ever trying to access this)
+            return
+        }
+        
+        do {
+            let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Follower].self, from: favoritesData)
+            completed(.success(favorites))
+            
+        } catch {
+            completed(.failure(.unableToFavorite))
+        }
+    }
+    
+    static func save(favorites: [Follower]) -> GFError? {
+        
+        do {
+            let encoder = JSONEncoder()
+            let encodedFavorites = try encoder.encode(favorites)
+            defaults.set(encodedFavorites, forKey: Keys.favorites) // similar to line 23, but now we are setting the object
+            return nil // returning nil, because there was no error
+            
+        } catch {
+            return .unableToFavorite
+        }
+        
+        
+    }
+}
--- GitHubFollowers/Utilities/ErrorMessage.swift
+++ GitHubFollowers/Utilities/ErrorMessage.swift
@@ -12,4 +12,6 @@ enum GFError: String, Error {
     case unableToComplete   = "Unable to complete your request. Please check your internet connection."
     case invalidResponse    = "Invalid response from the server. Please try again."
     case invalidData        = "The data received from the server is invalid. Please try again."
+    case unableToFavorite   = "There was an error fovoriting this user. Please try again."
+    case alreadyInFavorites = "You've already favorited this user."
 }
