1d550b478e40cf4e46eb9aed1f576fdded9a1d5f
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-03-09 22:53:51 +0100

    EmptyStateView code refactor and adapting to iPhoneSE. Quick DataLoadingVC code refactor

--- GitHubFollowers/Custom Views/ViewControllers/GFDataLoadingVC.swift
+++ GitHubFollowers/Custom Views/ViewControllers/GFDataLoadingVC.swift
@@ -36,8 +36,8 @@ class GFDataLoadingVC: UIViewController {
         //constraints:
         NSLayoutConstraint.activate([
             // center activityIndicator
-            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            activityIndicator.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: containerView.centerXAnchor)
         ])
         
         activityIndicator.startAnimating()
--- GitHubFollowers/Custom Views/Views/GFEmptyStateView.swift
+++ GitHubFollowers/Custom Views/Views/GFEmptyStateView.swift
@@ -27,26 +27,47 @@ class GFEmptyStateView: UIView {
     }
     
     private func configure() {
+        configureMessageLabel()
+        configureLogoImageView()
+    }
+    
+    private func configureMessageLabel() {
         addSubview(messageLabel)
-        addSubview(logoImageView)
         
         messageLabel.numberOfLines  = 3
         messageLabel.textColor      = .secondaryLabel
         
-        logoImageView.image         = Images.emptyStateLogo
-        logoImageView.translatesAutoresizingMaskIntoConstraints = false
         // !!! no messageLabel.translatesAutoresizingMaskIntoConstraints = false, because it is an instance of GFTitleLabel which has it set to false in GFTitleLabel.swift
         
+        let labelCenterYConstant: CGFloat = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed ? -80 : -150
+        let messageLabelCenterYConstraint = messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: labelCenterYConstant)
+        messageLabelCenterYConstraint.isActive = true
+        
         NSLayoutConstraint.activate([
-            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -150), // moving it up 150 points from the center to the right
+            // messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -150), // moving it up 150 points from the center to the right
             messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
             messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
             messageLabel.heightAnchor.constraint(equalToConstant: 200),
             
+        ])
+    }
+    
+    private func configureLogoImageView() {
+        addSubview(logoImageView)
+        
+        logoImageView.image         = Images.emptyStateLogo
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let logoBottomConstant: CGFloat = DeviceTypes.isiPhoneSE || DeviceTypes.isiPhone8Zoomed ? 80 : 40
+        let logoImageViewBottomConstraint = logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: logoBottomConstant)
+        logoImageViewBottomConstraint.isActive = true
+        
+        NSLayoutConstraint.activate([
+
             logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3), // take our image and make the width of the image 30% larger than the actual width of the screen
             logoImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
-            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170),
-            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 40)
+            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170)
+            // logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 40)
             
         ])
     }
