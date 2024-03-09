a255a5a56c0bc5d0307d9631af56e5d3514d6356
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-03-09 13:56:49 +0100

    Extract ContainerView config  from GFAlert to its own file

---
+++ GitHubFollowers/Custom Views/Views/GFAlertContainerView.swift
@@ -0,0 +1,28 @@
+//
+//  GFAlertContainerView.swift
+//  GitHubFollowers
+//
+//  Created by Paweł Rudnik on 09/03/2024.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        backgroundColor    = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth  = 2
+        layer.borderColor  = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
