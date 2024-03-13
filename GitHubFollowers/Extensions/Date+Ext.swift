9c4b9fdcbf180d2f21a9c8c67304a4a00780819c
Paweł Rudnik <pawelrudnik3@gmail.com>
2024-03-13 18:12:49 +0100

    Add a new way of formatting the data

--- GitHubFollowers/Extensions/Date+Ext.swift
+++ GitHubFollowers/Extensions/Date+Ext.swift
@@ -9,11 +9,17 @@ import Foundation
 
 extension Date {
     
+//    func convertToMonthYearFormat() -> String {
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "MMM d, yyyy"
+//        
+//        return dateFormatter.string(from: self) // "Jan 2013" format
+//    }
+    
     func convertToMonthYearFormat() -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MMM d, yyyy"
-        
-        return dateFormatter.string(from: self) // "Jan 2013" format
+        return formatted(.dateTime.month().year())                  // Jan 2008 format
+       // return formatted(.dateTime.month(.wide).year(.twoDigits)) // January 08 format
+       // return formatted(.dateTime.month(.twoDigits).year())      // 01/2008 format
     }
     
 }
